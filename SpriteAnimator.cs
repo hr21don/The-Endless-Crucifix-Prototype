a2f5c35b76c60e76aaed03f1f703a0575c1e52b0
hr18aba <76178801+hr18aba@users.noreply.github.com>
2021-04-05 13:56:50 +0100

    Add files via upload

---
+++ SpriteAnimator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class SpriteAnimator : MonoBehaviour
+{
+    [SerializeField] private Sprite[] frameArray;
+    private int currentFrame;
+    private float timer;
+    private readonly float framerate = 5f;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= framerate)
+        {
+            timer -= framerate;
+            currentFrame = (currentFrame + 1) % frameArray.Length;
+            gameObject.GetComponent<SpriteRenderer>().sprite = frameArray[currentFrame];
+
+        }
+    }
+
+
+
+}
